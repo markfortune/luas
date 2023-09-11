cdc4e75cc62c903ea54cd68f5542568a800601b7
Mark Fortune <51966164+markfortune@users.noreply.github.com>
2023-09-11 14:54:25 +0100

    Fixed __init__.py files

--- src/luas/__init__.py
+++ src/luas/__init__.py
@@ -3,5 +3,3 @@ from .GPClass import GP
 from .kernel_functions import *
 from .kronecker_functions import *
 from .LuasKernel import LuasKernel
-from .sample_kernels import *
-from .transit_functions import *
---
+++ src/luas/exoplanet/__init__.py
@@ -0,0 +1,2 @@
+from .sample_kernels import *
+from .transit_functions import *
